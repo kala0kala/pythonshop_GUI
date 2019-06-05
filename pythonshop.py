efe00755a80ad2d6a1d3f89e6cafce07e9fbdf2f
kala0kala <50321842+kala0kala@users.noreply.github.com>
2019-06-05 14:10:38 +0200

    Update pythonshop.py

--- pythonshop.py
+++ pythonshop.py
@@ -17,6 +17,8 @@ pasekMenu.add_cascade(label="Opcje" , menu = opcjeMenu)
 opcjeMenu.add_command(label = "Obrót 90" , command=obrot_90)
 opcjeMenu.add_command(label = "Obrót 180" , command=obrot_180)
 opcjeMenu.add_command(label = "Obrót 270" , command=obrot_270)
+opcjeMenu.add_command(label = "Skaluj" , command=opcja_skalowanie)
+opcjeMenu.add_command(label = "Czarno biały obraz" , command=czarno_bialy)
 
 glowneOkno.config(menu=pasekMenu)
 glowneOkno.mainloop()
