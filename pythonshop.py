a3838b5738c9f5118dc4d542b47fe5a91ae4f577
kala0kala <50321842+kala0kala@users.noreply.github.com>
2019-06-05 14:06:18 +0200

    menu PS

---
+++ pythonshop.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import messagebox
+
+glowneOkno = Tk()
+pasekMenu = Menu(glowneOkno)
+
+plikMenu = Menu(pasekMenu, tearoff = 0)
+pasekMenu.add_cascade(label="Plik" , menu = plikMenu)
+
+plikMenu.add_command(label = "Wczytaj plik" , command=wczytaj_plik)
+plikMenu.add_command(label = "Zapisz plik" , command=zapisz_plik)
+plikMenu.add_command(label = "Zamknij program" , command=zamknij)
+
+opcjeMenu = Menu(pasekMenu, tearoff = 0)
+pasekMenu.add_cascade(label="Opcje" , menu = opcjeMenu)
+
+opcjeMenu.add_command(label = "Obrót 90" , command=obrot_90)
+opcjeMenu.add_command(label = "Obrót 180" , command=obrot_180)
+opcjeMenu.add_command(label = "Obrót 270" , command=obrot_270)
+
+glowneOkno.config(menu=pasekMenu)
+glowneOkno.mainloop()
