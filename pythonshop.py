50468168bb1b27eff6a07539540730d3ec3a5e8f
kala0kala <50321842+kala0kala@users.noreply.github.com>
2019-06-07 10:44:54 +0200

    Update pythonshop.py

--- pythonshop.py
+++ pythonshop.py
@@ -1,7 +1,10 @@
+import os
 from tkinter import *
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
-def obrot_90(): #funkcja obracająca obraz o 90 stopni
+
+def obrot_90():
     global obraz
     obraz = obraz.rotate(90)
     plotno.obrazek=ImageTk.PhotoImage(obraz)
@@ -19,7 +22,6 @@ def obrot_270(): #fynkcja obracająca obraz o 270 stopni
     plotno.obrazek=ImageTk.PhotoImage(obraz)
     plotno.itemconfig(moj_obrazek, image=plotno.obrazek)
 
-#Tworzy płótno na którym wyświetla się obraz oraz dodaje obraz
 glowneOkno = Tk()
 pasekMenu = Menu(glowneOkno)
 plotno=Canvas(glowneOkno, width=400 ,height=400)
@@ -32,21 +34,20 @@ moj_obrazek=plotno.create_image(200 ,200 ,image=obrazTk)
 plikMenu = Menu(pasekMenu, tearoff = 0)
 pasekMenu.add_cascade(label="Plik" , menu = plikMenu)
 
-plikMenu = Menu(pasekMenu, tearoff = 0)
-pasekMenu.add_cascade(label="Plik" , menu = plikMenu) #w menu tworzy pasek opcji o nazwie "Plik"
-
-plikMenu.add_command(label = "Wczytaj plik" , command=wczytaj_plik) # w pasku "Plik" tworzy przycisk do wczytania pliku
-plikMenu.add_command(label = "Zapisz plik" , command=zapisz_plik) # w pasku "Plik" tworzy przycisk do zapisania pliku
-plikMenu.add_command(label = "Zamknij program" , command=zamknij) # w pasku "Plik" tworzy przycisk do zamknięcia programu
-
+'''
+plikMenu.add_command(label = "Wczytaj plik")
+plikMenu.add_command(label = "Zapisz plik" , command=zapisz_plik)
+plikMenu.add_command(label = "Zamknij program" , command=zamknij)
+'''
 opcjeMenu = Menu(pasekMenu, tearoff = 0)
-pasekMenu.add_cascade(label="Opcje" , menu = opcjeMenu) #w menu tworzy pasek opcji o nazwie "Opcje"
+pasekMenu.add_cascade(label="Opcje" , menu = opcjeMenu)
 
-opcjeMenu.add_command(label = "Obrót 90" , command=obrot_90) # w pasku "Opcje" tworzy przycisk do obrotu obrazu o 90 stopni
-opcjeMenu.add_command(label = "Obrót 180" , command=obrot_180) # w pasku "Opcje" tworzy przycisk do obrotu obrazu o 180 stopni
-opcjeMenu.add_command(label = "Obrót 270" , command=obrot_270) # w pasku "Opcje" tworzy przycisk do obrotu obrazu o 270 stopni
-opcjeMenu.add_command(label = "Skaluj" , command=opcja_skalowanie) # w pasku "Opcje" tworzy przycisk do skalowania obrazu
-opcjeMenu.add_command(label = "Czarno biały obraz" , command=czarno_bialy) # w pasku "Opcje" tworzy przycisk do zmiany kolorów obrazu na czarno-białe
-
+opcjeMenu.add_command(label = "Obrót 90" , command=obrot_90)
+opcjeMenu.add_command(label = "Obrót 180" , command=obrot_180)
+opcjeMenu.add_command(label = "Obrót 270" , command=obrot_270)
+'''
+opcjeMenu.add_command(label = "Skaluj" , command=opcja_skalowanie)
+opcjeMenu.add_command(label = "Czarno biały obraz" , command=czarno_bialy)
+'''
 glowneOkno.config(menu=pasekMenu)
 glowneOkno.mainloop()
