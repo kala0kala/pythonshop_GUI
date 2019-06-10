0519929c02c6bd6d38c03139eadb65e9a03441d8
kala0kala <50321842+kala0kala@users.noreply.github.com>
2019-06-10 14:33:56 +0200

    Update pythonshop.py

--- pythonshop.py
+++ pythonshop.py
@@ -5,7 +5,6 @@ from PIL import Image, ImageTk, ImageFilter
 from tkinter import filedialog
 
 
-
 def obrot_90(): #funkcja obracająca obraz o 90 stopni
     global obraz
     obraz = obraz.rotate(90)
@@ -99,7 +98,7 @@ def zamknij():
     
     
 glowneOkno = Tk()
-pasekMenu = Menu(glowneOkno)
+pasekMenu = Menu(glowneOkno) pasek menu
 
 plotno=Canvas(glowneOkno, width=1000 ,height=750)
 plotno.pack()
@@ -108,36 +107,34 @@ obrazTk=ImageTk.PhotoImage(obraz)
 plotno.obrazek=obrazTk
 moj_obrazek=plotno.create_image(200 ,200 ,image=obrazTk)
 
-plikMenu = Menu(pasekMenu, tearoff = 0) #paek menu
-pasekMenu.add_cascade(label="Plik" , menu = plikMenu)
+plikMenu = Menu(pasekMenu, tearoff = 0) #przycisk plik na pasku menu
+pasekMenu.add_cascade(label="Plik" , menu = plikMenu) #umożliwia się rozwinięcie przyciskowi plik
+
+plikMenu.add_command(label = "Wczytaj plik", command=wczytaj_plik) #przycisk umożliwiający wczytanie obrazu
+plikMenu.add_command(label = "Zapisz plik" , command=zapisz_plik) #przycisk umożliwiający zapisanie nowego obrazu
+plikMenu.add_command(label = "Zamknij program" , command=zamknij) #przycisk zamykający program
 
 
-plikMenu.add_command(label = "Wczytaj plik", command=wczytaj_plik)
-plikMenu.add_command(label = "Zapisz plik" , command=zapisz_plik)
-'''
-plikMenu.add_command(label = "Zamknij program" , command=zamknij)
-''' 
-
-opcjeMenu = Menu(pasekMenu, tearoff = 0)
-obrotObrazu = Menu(opcjeMenu, tearoff = 0)
+opcjeMenu = Menu(pasekMenu, tearoff = 0) #tworzy przycisk opcje w pasku menu
+obrotObrazu = Menu(opcjeMenu, tearoff = 0) 
 skalujObraz = Menu(opcjeMenu, tearoff = 0)
 filtryMenu=Menu(opcjeMenu, tearoff = 0)
 
-pasekMenu.add_cascade(label="Opcje" , menu = opcjeMenu)
-opcjeMenu.add_cascade(label="Obrót obrazu" , menu = obrotObrazu)
-obrotObrazu.add_command(label = "Obrót 90" , command=obrot_90)
-obrotObrazu.add_command(label = "Obrót 180" , command=obrot_180)
-obrotObrazu.add_command(label = "Obrót 270" , command=obrot_270)
+pasekMenu.add_cascade(label="Opcje" , menu = opcjeMenu) #umożliwia rozwinięcie się przyciskowi opcje
+opcjeMenu.add_cascade(label="Obrót obrazu" , menu = obrotObrazu) #Wumożliwia rozwinięcie się przyciskowi obrót obrazu
+obrotObrazu.add_command(label = "Obrót 90" , command=obrot_90) #przycisk wywołujący obrót obrazu o 90 stopni
+obrotObrazu.add_command(label = "Obrót 180" , command=obrot_180) #przycisk wywołujący obrót obrazu o 180 stopni
+obrotObrazu.add_command(label = "Obrót 270" , command=obrot_270) #przycisk wywołujący obrót obrazu o 270 stopni
 
-opcjeMenu.add_cascade(label = "Skaluj" , menu = skalujObraz)
-skalujObraz.add_command(label="101x150", command=opcje_skalowanie1)
-skalujObraz.add_command(label="150x212", command=opcje_skalowanie2)
-skalujObraz.add_command(label="214x232", command=opcje_skalowanie3)
+opcjeMenu.add_cascade(label = "Skaluj" , menu = skalujObraz #umożliwia rozwinięcie się przyciskowi skaluj
+skalujObraz.add_command(label="101x150", command=opcje_skalowanie1) #przycisk wywołujący skalowanie obrazu na wymiary 101x150 pikseli
+skalujObraz.add_command(label="150x212", command=opcje_skalowanie2) #przycisk wywołujący skalowanie obrazu na wymiary 150x212 pikseli
+skalujObraz.add_command(label="214x232", command=opcje_skalowanie3) #przycisk wywołujący skalowanie obrazu na wymiary 214x232 pikseli
 
-opcjeMenu.add_cascade(label="Filtry", menu=filtryMenu)
-filtryMenu.add_command(label="Czarno-biały", command=czarno_bialy)
-filtryMenu.add_command(label="Rozmycie", command=rozmycie)
-filtryMenu.add_command(label="Negatyw", command=emboss)
+opcjeMenu.add_cascade(label="Filtry", menu=filtryMenu) #umożliwia rozwinięcie się przyciskowi filtry
+filtryMenu.add_command(label="Czarno-biały", command=czarno_bialy) #przycisk wywołujący nałożenie filtra: czarno-biały
+filtryMenu.add_command(label="Rozmycie", command=rozmycie) #przycisk wywołujący nałożenie filtra: rozmycie
+filtryMenu.add_command(label="Negatyw", command=emboss) #przycisk wywołujący nałożenie filtra: negatyw
 
 glowneOkno.config(menu=pasekMenu)
 glowneOkno.mainloop()
